bfc34fa262d29f01425ff88f9665d2b73fe19c8d
krrishMangla <76586482+krrishMangla@users.noreply.github.com>
2021-03-04 14:00:12 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,25 @@
+class Block
+{
+  constructor(x,y,width,height)
+  {
+    var options = 
+    {
+     restitution:0.8,
+     friction:1.5,
+     density:1
+
+    }
+   this.body = Bodies.rectangle(x,y,width,height,options);
+   this.width = width;
+   this.height = height;
+   World.add(world, this.body);
+  }
+
+  display()
+  {
+    var pos = this.body.position;
+    rectMode(CENTER);
+    rect(pos.x,pos.y,this.width,this.height);
+  }
+
+}
